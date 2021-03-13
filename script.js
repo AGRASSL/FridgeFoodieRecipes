b6580f8daacfbc14885e7a5a5e4a55db76cbb7c0
Laura Oberwetter <leoberwetter@gmail.com>
2021-03-13 13:51:22 -0600

    first js edits

---
+++ script.js
@@ -0,0 +1,17 @@
+//VARIABLES
+var ingredients = $()
+var diet = $()
+
+//FUNCTION
+
+
+
+$(document).ready(function() { //prevents js from loading until the document is ready
+    $('.btn').on("click", function() { // activate enter button
+        ingredients.val(); // create ingredient variable
+        diet.val(); // create diet variable
+        // call AJAX function
+     });
+        //AJAX function {}
+});
+``

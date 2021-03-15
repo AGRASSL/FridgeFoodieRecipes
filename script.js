0640599e04534cdff2402776b35e508dd15710c9
Natalie Finnegan <nfinneg2@gmail.com>
2021-03-15 11:02:01 -0600

    search results html/css animations,more formatting

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ var diet = $()
 
 
 $(document).ready(function() { //prevents js from loading until the document is ready
-    $('.btn').on("click", function() { // activate enter button
-        ingredients.val(); // create ingredient variable
-        diet.val(); // create diet variable
+    $('.searchRecipes').on("click", function() { // activate enter button
+        
+        var searchResults = "search-results.html"
+
+        location.assign(searchResults);
+        
+        // ingredients.val(); // create ingredient variable
+        // diet.val(); // create diet variable
         // call AJAX function
      });
         //AJAX function {}
 });
-``

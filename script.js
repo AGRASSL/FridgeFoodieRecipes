8395f5717ae99c076f32db6f6a9ed1bbd3d9ce49
Natalie Finnegan <nfinneg2@gmail.com>
2021-03-15 19:24:39 -0600

    changed btns radio-checked

--- script.js
+++ script.js
@@ -4,18 +4,37 @@ var diet = $()
 
 //FUNCTION
 
-
-
 $(document).ready(function() { //prevents js from loading until the document is ready
-    $('.searchRecipes').on("click", function() { // activate enter button
-        
-        var searchResults = "search-results.html"
 
-        location.assign(searchResults);
-        
+$('.searchRecipes').on("click", function() { // activate enter button
+        var searchResults = "search-results.html"
+        location.assign(searchResults); 
         // ingredients.val(); // create ingredient variable
         // diet.val(); // create diet variable
         // call AJAX function
      });
         //AJAX function {}
+
+
+$('.goBack').on("click", function(){
+    var goBack = "index.html"
+    location.assign(goBack);
+})
+
+
+$('.secondRecipeBtn').on("click", function(){
+
+})
+
+
+var API = 'https://http://www.cors-anywhere.herokuapp.com/https://api.edamam.com/search?app_id=1c49a61b&api_key=db0145d0d0dd134bbf428353e18af69b&q=pizza'
+fetch(API)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+
+
 });

578cd02a1719cb4a9a9f08c07548968c94f1e065
Natalie Finnegan <nfinneg2@gmail.com>
2021-03-17 19:32:39 -0600

    local storage and fixed errors

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-//VARIABLES
-var ingredients = $();
-var diet = $();
-
-
 $(document).ready(function () {
   //prevents js from loading until the document is ready
-
 
   var ingredientsArr = [];
 
@@ -14,15 +8,6 @@ $(document).ready(function () {
     var searchResults = "search-results.html";
     location.assign(searchResults);
   });
-    
-
-  //AJAX function {}
-
-$('.getStarted').on("click", function() { // activate enter button
-        var searchResults = "search-results.html"
-        location.assign(searchResults); 
-       
-
 
   $(".goBack").on("click", function () {
     var goBack = "index.html";
@@ -30,59 +15,39 @@ $('.getStarted').on("click", function() { // activate enter button
   });
 
   $(".secondRecipeBtn").on("click", function () {
-    ingredientsArr.length = 0;
     let ingredient1 = $("#searchItem1").val();
     let ingredient2 = $("#searchItem2").val();
     let ingredient3 = $("#searchItem3").val();
-
+    let dietaryRes = $(".custom-select").val();
     ingredientsArr.push(ingredient1, ingredient2, ingredient3);
     console.log(ingredientsArr);
-    sendApiRequest(ingredientsArr);
+    console.log(dietaryRes);
+    window.localStorage.setItem("Ingredients", ingredientsArr);
+
+    sendApiRequest(ingredientsArr, dietaryRes);
   });
 
-  async function sendApiRequest(ingredientsArr) {
+  async function sendApiRequest(ingredientsArr, dietaryRes) {
     let APP_ID = "1c49a61b";
     let API_KEY = "db0145d0d0dd134bbf428353e18af69b";
     let response = await fetch(
       "https://api.edamam.com/search?q=" +
         ingredientsArr +
+        "&health=" +
+        dietaryRes +
         "&app_id=1c49a61b&app_key=db0145d0d0dd134bbf428353e18af69b"
     );
-    console.log(response);
     let data = await response.json();
-    console.log(data);
+  }
 
-    let dietaryRes = $(".custom-select").val();
-    console.log(dietaryRes);
-    ingredientsArr.push(ingredient1,ingredient2,ingredient3);
-    console.log(ingredientsArr);
-    sendApiRequest(ingredientsArr, dietaryRes);
-  
-})
-
-
-
-
-async function sendApiRequest(ingredientsArr, dietaryRes){
-    let APP_ID = "1c49a61b"
-    let API_KEY = "db0145d0d0dd134bbf428353e18af69b"
-    let response = await fetch("https://api.edamam.com/search?q=" + ingredientsArr +"&health=" + dietaryRes +"&app_id=1c49a61b&app_key=db0145d0d0dd134bbf428353e18af69b")
-    console.log(response)
-    let data = await response.json()
-    console.log(data)
-    useApiData(data)
-}
-
-// POPULATE CARD CONTENT
-    //API CALL
-    success: function (data) {
-        for (var i = 1; i < 10; i++) {
-            $("#cardTitle" + i).text(data[""][""]);
-            $("#cardInfo" + i).text(data[""][""]);
-            var cardLinkURL = ;
-            $("#cardLink" + i).attr('href', cardLinkURL);
-            var cardImgURL = ;
-            $("#cardImg" + i).attr('src', cardImgURL);
-        }
-    } }
+  // POPULATE CARD CONTENT
+  //API CALL
+  success: function testCard(data) {
+    for (var i = 1; i < 10; i++) {
+      $("#cardTitle" + i).text(data[""][""]);
+      $("#cardInfo" + i).text(data[""][""]);
+      var cardLinkURL = $("#cardLink" + i).attr("href", cardLinkURL);
+      var cardImgURL = $("#cardImg" + i).attr("src", cardImgURL);
+    }
+  }
 });

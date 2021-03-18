5b352cdff0f679e2f84d0db405cd9bb517462e71
Laura Oberwetter <leoberwetter@gmail.com>
2021-03-17 23:11:55 -0500

    card info populated

--- script.js
+++ script.js
@@ -1,53 +1,60 @@
 $(document).ready(function () {
-  //prevents js from loading until the document is ready
+    //prevents js from loading until the document is ready
 
-  var ingredientsArr = [];
+    var ingredientsArr = [];
 
-  $(".getStarted").on("click", function () {
-    // activate enter button
-    var searchResults = "search-results.html";
-    location.assign(searchResults);
-  });
+    $(".getStarted").on("click", function () {
+        // activate enter button
+        var searchResults = "search-results.html";
+        location.assign(searchResults);
+    });
 
-  $(".goBack").on("click", function () {
-    var goBack = "index.html";
-    location.assign(goBack);
-  });
+    $(".goBack").on("click", function () {
+        var goBack = "index.html";
+        location.assign(goBack);
+    });
 
-  $(".secondRecipeBtn").on("click", function () {
-    let ingredient1 = $("#searchItem1").val();
-    let ingredient2 = $("#searchItem2").val();
-    let ingredient3 = $("#searchItem3").val();
-    let dietaryRes = $(".custom-select").val();
-    ingredientsArr.push(ingredient1, ingredient2, ingredient3);
-    console.log(ingredientsArr);
-    console.log(dietaryRes);
-    window.localStorage.setItem("Ingredients", ingredientsArr);
+    $(".secondRecipeBtn").on("click", function () {
+        let ingredient1 = $("#searchItem1").val();
+        let ingredient2 = $("#searchItem2").val();
+        let ingredient3 = $("#searchItem3").val();
+        let dietaryRes = $(".custom-select").val();
+        ingredientsArr.push(ingredient1, ingredient2, ingredient3);
+        console.log(ingredientsArr);
+        console.log(dietaryRes);
+        window.localStorage.setItem("Ingredients", ingredientsArr);
 
-    sendApiRequest(ingredientsArr, dietaryRes);
-  });
+        sendApiRequest(ingredientsArr, dietaryRes);
+    });
 
-  async function sendApiRequest(ingredientsArr, dietaryRes) {
-    let APP_ID = "1c49a61b";
-    let API_KEY = "db0145d0d0dd134bbf428353e18af69b";
-    let response = await fetch(
-      "https://api.edamam.com/search?q=" +
-        ingredientsArr +
-        "&health=" +
-        dietaryRes +
-        "&app_id=1c49a61b&app_key=db0145d0d0dd134bbf428353e18af69b"
-    );
-    let data = await response.json();
-  }
+    async function sendApiRequest(ingredientsArr, dietaryRes) {
+        let APP_ID = "1c49a61b";
+        let API_KEY = "db0145d0d0dd134bbf428353e18af69b";
+        let response = await fetch(
+            "https://api.edamam.com/search?q=" +
+            ingredientsArr +
+            "&health=" +
+            dietaryRes +
+            "&app_id=1c49a61b&app_key=db0145d0d0dd134bbf428353e18af69b"
+        );
 
-  // POPULATE CARD CONTENT
-  //API CALL
-  success: function testCard(data) {
-    for (var i = 1; i < 10; i++) {
-      $("#cardTitle" + i).text(data[""][""]);
-      $("#cardInfo" + i).text(data[""][""]);
-      var cardLinkURL = $("#cardLink" + i).attr("href", cardLinkURL);
-      var cardImgURL = $("#cardImg" + i).attr("src", cardImgURL);
+        let data = await response.json();
+        console.log(data);
+
+        function cardPop(ingData) {
+            console.log(ingData);
+            
+            for (var i = 1; i < 10; i++) {
+                $("#cardTitle" + i).text(ingData["hits"][i]["recipe"]["label"]);
+                $("#cardInfo" + i).text(ingData["hits"][i]["recipe"]["source"]);
+                var cardLinkURL = (ingData["hits"][i]["recipe"]["url"]);
+                $("#cardLink" + i).attr("href", cardLinkURL); // create a property path 
+                var cardImgURL = (ingData["hits"][i]["recipe"]["image"]);
+                $("#cardImg" + i).attr("src", cardImgURL); // create a property path 
+            }
+        }
+        cardPop(data);
     }
-  }
+
+
 });
